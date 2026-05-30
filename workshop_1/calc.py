dc5a9732375d9c306ae1bc9c6107087da3657d3c
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2026-05-30 14:38:51 +0400

    Add workshop 1 code

---
+++ workshop_1/calc.py
@@ -0,0 +1,9 @@
+price = 20 # hard coded value
+quantity = 10
+
+total = quantity * price # dynamic value
+print("Total:", total, "GEL")
+
+# f-string / formatted strings
+text = f"Total: {total} GEL"
+print(text)
---
+++ workshop_1/main.py
@@ -0,0 +1,16 @@
+print("Hello")
+
+print(2 % 2)
+print(3 % 2)
+print(4 % 2)
+print(5 % 2)
+
+name = "Joe"
+age = "27"
+
+print(name)
+print(type(age + "3"))
+print(type(273))
+
+print(23 ** 1235)
+
